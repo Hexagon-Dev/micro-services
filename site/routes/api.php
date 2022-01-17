35824ac79a0944ea88f19c22019d84c8dd2a1ce4
hexagon14 <hexagon14@mail.ru>
2022-01-17 17:17:51 +0200

    Working with Traefik.

--- site/routes/api.php
+++ site/routes/api.php
@@ -1,1 +1,15 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
+
+Route::get('/pokemon/img/{name}', function ($name) {
+    $path = public_path('img') . '/' . $name;
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+    return $response;
+});
